cb3c7eecf5c1301fd52007d21a941f65ba7a65c5
Marian Vystavěl <mar1anmoon@seznam.cz>
2025-05-21 10:10:19 +0200

    react + boostrap první komponenta

--- unicornreact/src/App.js
+++ unicornreact/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Header from './Header';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <h2>Welcome to the Unicorn React App</h2>
+        <p>This is a simple React application with Bootstrap styling.</p>
+        <p>Feel free to explore and customize it!</p>
+      </main>
     </div>
   );
+ 
 }
 
 export default App;
